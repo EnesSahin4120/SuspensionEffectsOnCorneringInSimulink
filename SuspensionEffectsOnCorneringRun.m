7b894a298d42b0ddfd539ddd0f4b701639bfe8cc
Enes Sahin <enessahin2022@outlook.com>
2024-10-18 01:01:11 +0300

    V0.0

---
+++ SuspensionEffectsOnCorneringRun.m
@@ -0,0 +1,21 @@
+Sim_Time = 2;
+g = 9.81;
+
+M = 275;
+k = 100000;
+b = 3000;
+SpringLength = 0.4;
+
+VehicleMass = 1100;
+SeparationBetweenSprings = 1.475;
+TrackWidth = 1.5;
+hr = 0.1;
+h1 = 0.45;
+
+BackRightLateral = 1200;
+BackLeftLateral = 1225;
+FrontRightLateral = 1600;
+FrontLeftLateral = 1650;
+CurvatureRadius = 600;
+
+sim("SuspensionEffectsOnCornering.slx")
